76002ca945eebc38ead6d87f377e0ebfaefa1b84
Dina Kontsevich <dina@Dinas-MacBook-Pro.local>
2025-05-10 13:29:26 +0300

    changed the design

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-// Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
